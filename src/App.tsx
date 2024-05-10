5df5cbb6741c897fec87dcc55b110c746e9e80ae
aiXpertLab <aiXpertLab@gmail.com>
2024-05-09 21:28:39 -0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vi           te and React logos to learn more
+        dddClick on the learn more
       </p>
     </>
   )
